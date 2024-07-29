c5d36445924f6ff87f08f2f70cab521a63eafe62
Steven Sermeus <stevensermeus@gmail.com>
2024-07-29 17:38:28 +0200

    Update package json to match package and start contruct of the client

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+import * as net from "net";
+
+class GoVal {
+  private host: string;
+  private port: number;
+  private passphrase: string;
+  private bufferSize: number;
+  private client: net.Socket;
+
+  constructor(
+    host: string,
+    port: number,
+    passphrase: string,
+    bufferSize?: number
+  ) {
+    this.host = host;
+    this.port = port;
+    this.passphrase = passphrase;
+    this.bufferSize = bufferSize || 1024;
+    this.client = new net.Socket();
+    this.client.connect(this.port, this.host, () => {
+      console.log("Connected");
+    });
+    this.client.write(this.passphrase);
+  }
+}
